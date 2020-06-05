44c1ca33110f3af5badb9a21309aaf3051e9ab36
nasirin50 <ahmadnasirin50@gmail.com>
2020-06-05 12:52:14 +0700

    membuat data statistik di home backend

--- app/Http/Controllers/backend/HomeController.php
+++ app/Http/Controllers/backend/HomeController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\TravelPackages;
+use App\Transaction;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        return view('backend.pages.home');
+        return view('backend.pages.home', [
+            'travel_package' => TravelPackages::count(),
+            'transaction' => Transaction::count(),
+            'transaction_pending' => Transaction::where('transaction_status','PENDING')->count(),
+            'transaction_success' => Transaction::where('transaction_status','SUCCESS')->count()
+        ]);
     }
 }
--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -12,7 +12,7 @@
     <hr class="sidebar-divider my-0">
 
     <li class="nav-item active">
-        <a class="nav-link" href="#">
+        <a class="nav-link" href="{{url('/admin')}}">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
