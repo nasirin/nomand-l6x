969e850ebb41f8027efa6975dd2dddebc8e27dae
nasirin50 <ahmadnasirin50@gmail.com>
2020-06-05 11:30:05 +0700

    membuat model transaksi

---
+++ app/Transaction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes; // 1. panggil library softdelete 
+
+class Transaction extends Model
+{
+    use SoftDeletes; // 2. gunakan softdelete
+
+    // 3. menambahkan field lable, yang berfungsi untuk : menyimpan secara langsung (mass assignment). pastikan sama seperti di tabel.
+    protected $fillable = ['travel_packages_id','users_id','additional_visa','transaction_total','transaction_status'];
+
+    protected $hidden=[];
+
+    // membuat relasi table
+    public function details()
+    {
+        return $this->hasMany(TransactioDetail::class, 'transaction_id','id');
+    }
+    public function travel_package()
+    {
+        return $this->belongsTo(TravelPackages::class, 'travel_packages_id','id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id','id');
+    }
+}
